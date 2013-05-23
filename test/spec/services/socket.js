a281dbfcd89e17c7490688d0c6560f93edf67ed2
Agustin Treceno <agustin.treceno@gmail.com>
2013-05-23 11:56:45 +0100

    Add socket service test

---
+++ test/spec/services/socket.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Service: socket', function () {
+
+    // load the service's module
+    beforeEach(module('stadion'));
+
+    // TODO: Implement this test
+    /*
+    // instantiate service
+    var socket;
+    beforeEach(inject(function (_socket_) {
+        socket = _socket_;
+    }));
+    */
+    it('should do something', function () {
+//        expect(!!socket).toBe(true);
+        expect(true).toBe(true);
+    });
+
+});
