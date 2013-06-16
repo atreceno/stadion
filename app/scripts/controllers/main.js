6274b96c569f95871904bcc53904e8322b49e103
Agustin Treceno <agustin.treceno@gmail.com>
2013-06-16 16:26:01 +0100

    Modify contact modal and fix style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,57 @@
 'use strict';
 
-angular.module('stadion')
-    .controller('MainCtrl', function ($scope, Feedback) {
-        $scope.open = function () {
-            $scope.shouldBeOpen = true;
-            $scope.feedback = {name: '', email: '', message: ''};
-        };
-        $scope.close = function () {
+var stadion = angular.module('stadion');
+stadion.controller('MainCtrl', function ($scope, $dialog, Feedback) {
+    $scope.open = function () {
+        $scope.shouldBeOpen = true;
+        $scope.feedback = {name: '', email: '', message: ''};
+    };
+    $scope.close = function () {
+        $scope.shouldBeOpen = false;
+    };
+    $scope.opts = {
+        backdrop: true, 
+        keyboard: true,
+        backdropClick: true,
+        dialogFade: true,
+        backdropFade: true,
+        templateUrl: 'partials/feedback.html',
+        controller: 'DialogCtrl'
+    };
+    $scope.opts2 = {
+        backdropFade: true, 
+        dialogFade: true
+    };
+    $scope.send = function () {
+        Feedback.save($scope.feedback, function () {
+            console.log('Feedback sent successfully');
             $scope.shouldBeOpen = false;
-        };
-        $scope.opts = {
-            backdropFade: true, 
-            dialogFade: true
-        };
-        $scope.send = function () {
-            Feedback.save($scope.feedback, function () {
-                console.log('Feedback sent successfully');
-                $scope.shouldBeOpen = false;
-            });
-        };
-    });
+        });
+    };
+    $scope.openFb = function () {
+        $dialog.dialog($scope.opts).open().then(function(name) {
+            alert(name);
+        });
+    };
+});
+stadion.controller('DialogCtrl', function ($scope, dialog) {
+    $scope.closeFb = function (name) {
+        dialog.close(name);
+    };
+    $scope.sendFb = function (data) {
+        Feedback.save($scope.feedback, function () {
+            console.log('Feedback sent successfully');
+     //       $scope.shouldBeOpen = false;
+        });
+        dialog.close(data);
+    };
+});
+stadion.controller('bmCtrl', function ($scope, Feedback) {
+    $scope.send = function () {
+        Feedback.save($scope.feedback, function () {
+            console.log('Feedback sent successfully');
+            $scope.isSent = true;
+        });
+    };
+});
+
